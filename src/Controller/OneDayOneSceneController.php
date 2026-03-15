88148937afc4bf7c4f90e87502f4dea37dd1ab37
0930yuyaikeda <0930arnaud@gmail.com>
2026-03-15 23:51:05 +0700

    yuyaikeda 20260315 画面モックを作成

--- src/Controller/OneDayOneSceneController.php
+++ src/Controller/OneDayOneSceneController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 // Entity
 use App\Entity\Admins;
@@ -10,13 +12,23 @@ use App\Entity\Admins;
 // Repository
 use App\Repository\AdminsRepository;
 
-
 class OneDayOneSceneController extends AbstractController
 {
     public function index(
         AdminsRepository $adminRepository
     ): Response
     {
-        dd('Hello world!');
+        $adminCount = count($adminRepository->findAll());
+
+    //    return $this->render('index.html.twig',[]);
+    //    return $this->render('inputName.html.twig',[]);
+    //    return $this->render('chooseDecks.html.twig',[]);
+    //    return $this->render('checkDeckAndName.html.twig',[]);
+    //    return $this->render('question.html.twig',[]);
+    //    return $this->render('result.html.twig',[]);
+    //    return $this->render('scriptDescription.html.twig',[]);
+    //    return $this->render('chooseScene.html.twig',[]);
+    //    return $this->render('countdown.html.twig',[]);
+       return $this->render('performance.html.twig',[]);
     }
 }
